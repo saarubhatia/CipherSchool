5c5b02d6a9d156bd54b78cd7ba480fc639f1725f
Saaru Bhatia <saarubhatia59@gmail.com>
2020-08-13 12:51:13 +0530

    Comments added for better understanding

--- Classwork_tasks/stack.js
+++ Classwork_tasks/stack.js
@@ -2,17 +2,25 @@ class Stack {
     constructor(){ 
         this.items = []; 
     } 
+    //add elements in the stack
     push(element){ 
         this.items.push(element); 
     } 
+    //deletes elements from the stack
     pop(){ 
         if(this.items.length == 0) 
             return "Underflow"; 
         return this.items.pop(); 
     } 
+    //Checks If the Stack Is Empty
     isEmpty(){ 
         return this.items.length == 0; 
     } 
+    //returns the top element of the stack
+    peek(){
+         return this.items[this.items.length - 1]; 
+    } 
+    //print the elements
     display(){ 
         var str = ""; 
         for (var i = 0; i < this.items.length; i++) 
@@ -26,5 +34,6 @@ console.log(stack.isEmpty());
 stack.push(10); 
 stack.push(20); 
 stack.push(30); 
+console.log(stack.peek());
 console.log(stack.pop());  
 console.log(stack.display())
